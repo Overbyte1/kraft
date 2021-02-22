d88add5a945acf2ca5405b5cb603504ef8802422
lin <2454123745@qq.com>
2021-02-22 21:32:34 +0800

    election configuration for test

--- kraft-core/src/main/java/election/config/GlobalConfig.java
+++ kraft-core/src/main/java/election/config/GlobalConfig.java
@@ -1,4 +1,50 @@
 package election.config;
-
+//参数，用于测试
 public class GlobalConfig {
+    //单位是毫秒
+    private int minElectionTimeout = 3000;
+    private int maxElectionTimeout = 4000;
+    private int logReplicationInterval = 1000;
+    private int logReplicationResultTimeout = 1000;
+    private int connectTimeout = 800;
+
+    public int getConnectTimeout() {
+        return connectTimeout;
+    }
+
+    public void setConnectTimeout(int connectTimeout) {
+        this.connectTimeout = connectTimeout;
+    }
+
+    public int getMinElectionTimeout() {
+        return minElectionTimeout;
+    }
+
+    public void setMinElectionTimeout(int minElectionTimeout) {
+        this.minElectionTimeout = minElectionTimeout;
+    }
+
+    public int getMaxElectionTimeout() {
+        return maxElectionTimeout;
+    }
+
+    public void setMaxElectionTimeout(int maxElectionTimeout) {
+        this.maxElectionTimeout = maxElectionTimeout;
+    }
+
+    public int getLogReplicationInterval() {
+        return logReplicationInterval;
+    }
+
+    public void setLogReplicationInterval(int logReplicationInterval) {
+        this.logReplicationInterval = logReplicationInterval;
+    }
+
+    public int getLogReplicationResultTimeout() {
+        return logReplicationResultTimeout;
+    }
+
+    public void setLogReplicationResultTimeout(int logReplicationResultTimeout) {
+        this.logReplicationResultTimeout = logReplicationResultTimeout;
+    }
 }
